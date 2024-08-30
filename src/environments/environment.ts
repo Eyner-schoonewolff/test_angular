85a95b2f083311e5de79c8a87667608ad8f9bda1
Eyner-schoonewolff <enewolff2013@gmail.com>
2024-08-30 07:37:45 -0500

    produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiURL: 'http://127.0.0.1:8000/api/v1',
+    apiURL: 'https://test-prueba-python-3.onrender.com/api/v1',
     apiKey: '123456-DEV'
 };
 
